990c5151b3559f5b32bd1fdfe23c899181418309
sakib3 <sakib3@gmail.com>
2016-11-13 01:06:13 +0100

    modified port variable

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("*",function(req,res){
 	res.status(400).send({ error: 'Not found' });
 });
 
-var port = process.env.NODE_ENV == 'production' ? process.env.PROD_PORT : config.serverPORT[process.env.NODE_ENV];
+var port = process.env.NODE_ENV == 'production' ? process.env.PORT || process.env.PROD_PORT : config.serverPORT[process.env.NODE_ENV];
 
 app.listen(port);
 console.log('Running on port '+ port+' ....');
